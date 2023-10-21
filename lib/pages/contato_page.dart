4c7d1a112c5adc7cda34bb0404dabf4ff918c45a
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2023-10-21 15:16:52 -0300

    feat: Criar tela de contato

---
+++ lib/pages/contato_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContatoPage extends StatefulWidget {
+  final String? contatoId;
+
+  const ContatoPage({super.key, required this.contatoId});
+
+  @override
+  State<ContatoPage> createState() => _ContatoPageState();
+}
+
+class _ContatoPageState extends State<ContatoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,80 +1,146 @@
+import 'package:agenda/models/contato_model.dart';
+import 'package:agenda/pages/contato_page.dart';
+import 'package:agenda/services/back4App/back4app_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  var carregando = false;
+  var contatoModel = ContatoModel([]);
+  var service = Back4AppService();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    carregarDados();
+  }
+
+  carregarDados() async {
+    if (mounted) {
+      setState(() {
+        carregando = true;
+      });
+
+      contatoModel = await service.get(null);
+
+      setState(() {
+        carregando = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: const Text("Contatos"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      body: carregando
+          ? const Center(child: CircularProgressIndicator())
+          : contatoModel.results == null || contatoModel.results!.isEmpty
+              ? const Center(child: Text("Nenhum contato encontrado"))
+              : ListView.builder(
+                  itemCount: contatoModel.results!.length,
+                  itemBuilder: (_, int index) {
+                    return Dismissible(
+                      direction: DismissDirection.startToEnd,
+                      onDismissed: (DismissDirection dismissDirection) async {
+                        if (dismissDirection == DismissDirection.startToEnd) {
+                          try {
+                            await service
+                                .delete(contatoModel.results![index].objectId!);
+                            carregarDados();
+                            if (mounted) {
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text('Contato excluído com sucesso!'),
+                                elevation: 8,
+                                backgroundColor: Colors.green,
+                              ));
+                            }
+                          } catch (e) {
+                            if (mounted) {
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(SnackBar(
+                                content: Text(e.toString()),
+                                elevation: 8,
+                                backgroundColor: Colors.red,
+                              ));
+                            }
+                          }
+                        }
+                      },
+                      key: Key(contatoModel.results![index].objectId!),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 16),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext bc) => ContatoPage(
+                                        contatoId: contatoModel
+                                            .results![index].objectId!)));
+                          },
+                          child: Card(
+                            elevation: 10,
+                            shadowColor: Colors.red,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                            "Estado: ${contatoModel.results![index].nome ?? ""}",
+                                            style: const TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500)),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                            "Cidade: ${contatoModel.results![index].email ?? ""}",
+                                            style: const TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500)),
+                                      ),
+                                    ],
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                        contatoModel.results![index].email ??
+                                            "",
+                                        style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+    ));
   }
 }
